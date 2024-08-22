1b7ab917543af74738cd61b8c5863e2dfe82fed3
AvivaNisan <avivanisan@gmail.com>
2024-08-22 04:21:56 +0000

    added conditional to compare the ages of two different people

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // all code for this lab to be completed in this file
+let currentTemperature = 78 
+
+//is the temp freezing?
+if (currentTemperature > 32) {
+    console.log("im not burning hot")
+} else {
+    console.log("i really like cold rooms")
+}
+
+let agePerson1 = 25;
+let agePerson2 = 30;
+
+if (agePerson1 > agePerson2) {
+    console.log("Person 1 is older than Person 2.");
+} else if (agePerson1 < agePerson2) {
+    console.log("Person 1 is younger than Person 2.");
+} else {
+    console.log("Person 1 and Person 2 are the same age.");
+}
